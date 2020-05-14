156cb98e33a09585ada8e4bccab9ad7d88eeb5dc
BenderCity <10crafter10@gmail.com>
2020-05-14 18:01:05 +0300

    Menu2

---
+++ Myfirstgame/Assets/Menu/Scripts/Exit.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Exit : MonoBehaviour
+{
+    public Button exitButton;
+    void Start()
+    {
+        exitButton.onClick.AddListener(TaskOnClick);
+
+
+    }
+
+    void TaskOnClick()
+    {
+        Application.Quit();
+    }  
+}
---
+++ Myfirstgame/Assets/Menu/Scripts/Settings.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class Settings : MonoBehaviour {
+
+
+    public void ExitGame() {
+        Application.Quit();
+    }
+
+	public void LoadLevel (int numScene)
+    {
+        SceneManager.LoadScene(numScene);
+    }
+		
+	
+    public void ShowPanel (GameObject obj)
+    {
+        obj.SetActive(true);
+    }
+    public void HidePanel (GameObject obj)
+    {
+        obj.SetActive(false);
+    }
+}
